43a73fc9e4fd1b80077bd99119369f7b7bdd5e1e
Wenslaus-Makumba <wenslausmakumba2020@gmail.com>
2020-07-13 02:55:58 +0300

    added the app file that contains app components

---
+++ app/models.py
@@ -0,0 +1,35 @@
+class Articles:
+    '''
+    Articles class to define articles objects
+    '''
+    def __init__(self,id,author,title,description,url,image,):
+        self.id = id
+        self.author = author
+        self.title = title
+ 
+
+
+
+
+
+
+class Sources:
+    '''
+    Sources class to define Sources Objects
+    '''
+
+    def __init__(self,id,name,d):
+        self.id =id
+        self.name = name
+        self.url = url
+    
+
+
+
+
+
+
+
+
+
+
